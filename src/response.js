80b63ad79cb69f945a122fc93dcea4fe03ef1357
Ian Del Duca <ianmdelduca@gmail.com>
2020-08-17 18:24:44 -0400

    update response

--- src/response.js
+++ src/response.js
@@ -2,16 +2,17 @@ export const companies = [
   {
     name: "Google",
     contacts: [
-      { displayName: "John Smith", email: "JohnSmith@goole.com" },
-      { displayName: "Virginia Dare", email: "VADare@goole.com" },
-      { displayName: "Bobby Jones", email: "Bobby@goole.com" },
+      { displayName: "John Smith", email: "JohnSmith@google.com" },
+      { displayName: "Virginia Dare", email: "VADare@google.com" },
+      { displayName: "Larry Page", email: "Bobby@google.com" },
+      { displayName: "Bobby Jones", email: "Bobby@google.com" },
     ],
   },
   {
     name: "Facebook",
     contacts: [
       { displayName: "Mark Zuckerberg", email: "zucksux@facebook.com" },
-      { displayName: "Sheryl Sanberg", email: "leanin@facebook.com" },
+      { displayName: "Sheryl Sandberg", email: "leanin@facebook.com" },
       { displayName: "Joe Schmoe", email: "jschmoe@facebook.com" },
     ],
   },
