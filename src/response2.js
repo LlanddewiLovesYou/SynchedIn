5716bd3c6e92612877ddb47deab3f9ebcd94abea
Ian Del Duca <ianmdelduca@gmail.com>
2020-08-17 21:20:31 -0400

    Add mock data

--- src/response2.js
+++ src/response2.js
@@ -1,18 +1,18 @@
 export const contacts = [
   {
-    linkedinId: "uuid-1",
+    linkedinId: "uuid-3",
     displayName: "Mark Zuckerburg",
     company: "Facebook",
   },
   {
-    linkedinId: "uuid-2",
-    displayName: "Sheryl Sandberg",
-    company: "Facebook",
+    linkedinId: "uuid-13",
+    displayName: "James Barnes",
+    company: "Amazon",
   },
   {
-    linkedinId: "uuid-3",
-    displayName: "Joe Schmoe",
-    company: "Facebook",
+    linkedinId: "uuid-9",
+    displayName: "Jane Doe",
+    company: "Spotify",
   },
   {
     linkedinId: "uuid-4",
@@ -20,9 +20,9 @@ export const contacts = [
     company: "Google",
   },
   {
-    linkedinId: "uuid-5",
-    displayName: "John Smith",
-    company: "Google",
+    linkedinId: "uuid-1",
+    displayName: "Joe Schmoe",
+    company: "Facebook",
   },
   {
     linkedinId: "uuid-6",
@@ -30,43 +30,43 @@ export const contacts = [
     company: "Google",
   },
   {
+    linkedinId: "uuid-11",
+    displayName: "Jeff Bezos",
+    company: "Amazon",
+  },
+  {
     linkedinId: "uuid-7",
     displayName: "Bobby Jones",
     company: "Google",
   },
   {
-    linkedinId: "uuid-1",
-    displayName: "Mark Zuckerburg",
-    company: "Facebook",
+    linkedinId: "uuid-8",
+    displayName: "Steve Stephens",
+    company: "Spotify",
+  },
+  {
+    linkedinId: "uuid-5",
+    displayName: "John Smith",
+    company: "Google",
+  },
+  {
+    linkedinId: "uuid-12",
+    displayName: "Bill Yonairre",
+    company: "Amazon",
+  },
+  {
+    linkedinId: "uuid-10",
+    displayName: "Tori Amos",
+    company: "Spotify",
+  },
+  {
+    linkedinId: "uuid-14",
+    displayName: "Suzie Noble",
+    company: "Amazon",
   },
   {
     linkedinId: "uuid-2",
     displayName: "Sheryl Sandberg",
     company: "Facebook",
-  },
-  {
-    linkedinId: "uuid-3",
-    displayName: "Joe Schmoe",
-    company: "Facebook",
-  },
-  {
-    linkedinId: "uuid-4",
-    displayName: "Larry Page",
-    company: "Google",
-  },
-  {
-    linkedinId: "uuid-5",
-    displayName: "John Smith",
-    company: "Google",
-  },
-  {
-    linkedinId: "uuid-6",
-    displayName: "Virginia Dare",
-    company: "Google",
-  },
-  {
-    linkedinId: "uuid-7",
-    displayName: "Bobby Jones",
-    company: "Google",
   },
 ];
