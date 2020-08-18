e3830daea0a43145214681cd4970be1d78f5242e
Ian Del Duca <ianmdelduca@gmail.com>
2020-08-17 23:05:01 -0400

    rename form component

--- src/components/SubmissionForm/SubmissionForm.tsx
+++ src/components/SubmissionForm/SubmissionForm.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./MessageForm.scss";
+import React, { useState } from "react";
+import "./SubmissionForm.scss";
 import { Selector } from "../ContactSelector/ContactSelector";
 import { contacts } from "../../response2.js";
 
@@ -8,11 +8,12 @@ export interface Props {
 }
 
 export const MessageForm: React.FC<Props> = ({ children }) => {
+  const [formState, setFormState] = useState({ message: "", recipients: [] });
   return (
     <form onSubmit={() => console.log("submitted")} className="message-form">
       <div className="message-form__box">
         {children}
-        <Selector contacts={contacts}></Selector>
+        <Selector contacts={contacts} setFormState={setFormState}></Selector>
       </div>
       <button>Send Messages</button>
     </form>
